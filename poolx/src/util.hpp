5ec291be20c51d7ee1b4b440dca2f9a42bf5550d
Alessandro Contenti <a.contenti@gmail.com>
2021-01-04 21:46:54 +0100

    fix util.hpp
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- poolx/src/util.hpp
+++ poolx/src/util.hpp
@@ -1,15 +1,12 @@
 #pragma once
 
-#include <experimental/source_location>
-
-using source_location = std::experimental::source_location;
-
 class execution_error : public std::runtime_error {
 public:
-	explicit execution_error(const std::string &message, const source_location &location = source_location::current())
-			: runtime_error(std::string(location.file_name()) + ":" + std::to_string(location.line()) + ":" +
-							std::to_string(location.column()) + ": " + message) {}
+	execution_error(const std::string &file, int line, const std::string &message) :
+			runtime_error(file + ":" + std::to_string(line) + ": " + message) {}
 };
+
+#define execution_error(message) execution_error(__FILE__, __LINE__, message)
 
 static bool endsWith(std::string_view str, std::string_view suffix) {
 	return str.size() >= suffix.size() && 0 == str.compare(str.size() - suffix.size(), suffix.size(), suffix);
